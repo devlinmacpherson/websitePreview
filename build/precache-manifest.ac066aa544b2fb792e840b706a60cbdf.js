3d6c66f416b61d15f304f5a9ac0dc0066525d195
devlinmacpherson <dev.macpherson@gmail.com>
2022-06-17 15:44:00 -0400

    init

---
+++ build/precache-manifest.ac066aa544b2fb792e840b706a60cbdf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc91022b9ae8c2d02e2a0c2691efd407",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05be3e2c38feb570fa2d",
+    "url": "/static/css/main.6d67d770.chunk.css"
+  },
+  {
+    "revision": "6eeb96051c9bf120c225",
+    "url": "/static/js/2.0fa8dfa3.chunk.js"
+  },
+  {
+    "revision": "3a3eee750260ab555627e944270db51c",
+    "url": "/static/js/2.0fa8dfa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05be3e2c38feb570fa2d",
+    "url": "/static/js/main.e6158e6a.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "679cc95be9f53cdb7c9d90ef95ca9938",
+    "url": "/static/media/illustration-section-01.679cc95b.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "47fbacad3d12cdbfccdc179780b46767",
+    "url": "/static/media/logo.47fbacad.svg"
+  },
+  {
+    "revision": "2356967d07aef7b3843b346cfd691014",
+    "url": "/static/media/nfc.2356967d.png"
+  },
+  {
+    "revision": "d4987787a89b579a4dded398a1d64805",
+    "url": "/static/media/powercube.d4987787.png"
+  },
+  {
+    "revision": "44f52e870a9ea79562688afb3ad96d62",
+    "url": "/static/media/process-build.44f52e87.png"
+  },
+  {
+    "revision": "63991be1681b6257f7c8d403ed19ae3f",
+    "url": "/static/media/process-design.63991be1.png"
+  },
+  {
+    "revision": "f090e1773fe8989aeeab5b1a2c066d39",
+    "url": "/static/media/process-prototype.f090e177.png"
+  },
+  {
+    "revision": "4a503d51b8b2cb5c6fc641aa1b74b771",
+    "url": "/static/media/snowflake.4a503d51.png"
+  },
+  {
+    "revision": "d7665f4abbf987e3e4626391d5af637f",
+    "url": "/static/media/videoBg.d7665f4a.mp4"
+  }
+]);
